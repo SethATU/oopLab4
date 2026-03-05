bffb7371487c2bf0fbde3878fbd6ab73e3b48566
SuperButler <g00467621@atu.ie>
2026-03-05 15:11:25 +0000

    initial commit

---
+++ src/main/java/ie/atu/ooplab4/OopLab4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.ooplab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopLab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/ooplab4/OopLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.ooplab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopLab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

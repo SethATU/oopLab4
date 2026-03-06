67a96924df23fffd04183a5ed07a0f9054c4f17a
SuperButler <g00467621@atu.ie>
2026-03-06 23:45:39 +0000

    made own files

--- src/main/java/ie/atu/ooplab4/model/Calorie.java
+++ src/main/java/ie/atu/ooplab4/model/Calorie.java
@@ -1,4 +1,16 @@
 package ie.atu.ooplab4.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Calorie {
+    private long id;
+    @NotBlank(message = "Item name required")
+    private String item;
+    @Positive(message = "Calorie must be greater than 0")
+    private int cal;
 }
--- src/main/java/ie/atu/ooplab4/service/CalorieService.java
+++ src/main/java/ie/atu/ooplab4/service/CalorieService.java
@@ -1,4 +1,22 @@
 package ie.atu.ooplab4.service;
 
+import ie.atu.ooplab4.model.Calorie;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CalorieService {
+    private final List<Calorie> calories = new ArrayList<>();
+    private long nextId = 1;
+
+    public Calorie createCalorie(Calorie calorie) {
+        calorie.setId(nextId++);
+        calories.add(calorie);
+        return calorie;
+    }
+    public List<Calorie> getCalories() {
+        return calories;
+    }
 }
